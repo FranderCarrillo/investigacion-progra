4805e15984ff1e1b2218f91efc8a10521b306a29
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-10-05 19:41:21 -0600

    cambios de frander

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-// GET Request
+// se asigna el evento de clic al botón para obtener datos
 document.querySelector('#getButton').addEventListener('click', getData);
+
 function getData() {
-const resultBox = document.querySelector('#getResult');
-resultBox.textContent = 'Running GET request...';
-fetch('https://jsonplaceholder.typicode.com/photos/2')
-    .then(response => response.json())
-    .then(data => {
-    resultBox.textContent = JSON.stringify(data, null, 2);
-    console.log(resultBox)
-    })
-    .catch(error => {
-    resultBox.textContent = 'Error fetching data: ' + error;
-    });
+    const resultBox = document.querySelector('#getResult'); // selecciona el elemento para mostrar los resultados
+    resultBox.textContent = 'Running GET request...'; // Mensaje inicial mientras se realiza la solicitud
+
+    // realiza la solicitud GET a la API para obtener el objeto con ID 2
+    fetch('https://jsonplaceholder.typicode.com/photos/2')
+        .then(response => response.json()) // Convertir la respuesta en formato JSON
+        .then(data => {
+            resultBox.textContent = JSON.stringify(data, null, 2); // Mostrar el objeto JSON en el elemento resultBox
+            console.log(resultBox); // se muestra el elemento resultBox en la consola
+            console.log(data); // se muestra el objeto JSON obtenido en la consola
+        })
+        .catch(error => {
+            // manejo de errores de la solicitud
+            resultBox.textContent = 'Error fetching data: ' + error; // Mostrar un mensaje de error en resultBox
+        });
 }
+
